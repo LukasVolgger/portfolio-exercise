e75abcab04cb2ba12e2cb14e31f93ae00c887dff
Lukas Volgger <55922592+LukasVolgger@users.noreply.github.com>
2022-08-30 20:14:48 +0200

    add privacy policy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { DataProtectionComponent } from './data-protection/data-protection.component';
 
 const routes: Routes = [
   { path: '', component: SlideshowComponent },
-  { path: 'imprint', component: ImprintComponent }
+  { path: 'imprint', component: ImprintComponent },
+  { path: 'data-protection', component: DataProtectionComponent }
 ];
 
 @NgModule({
